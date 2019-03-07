92a006d0a14dc1dec1e7d948616908ad19c85700
texacoson <jani.hyrkas@gmail.com>
2019-03-07 14:11:21 +0200

    Early app, second commit using android studio vcs.

--- app/src/main/java/com/example/mychatclientapp/ChatAdapter.kt
+++ app/src/main/java/com/example/mychatclientapp/ChatAdapter.kt
@@ -2,22 +2,27 @@ package com.example.mychatclientapp
 
 import android.support.v7.widget.AppCompatAutoCompleteTextView
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 
-class ChatAdapter(private val dataSet: Array<String>) : RecyclerView.Adapter<ChatAdapter.MyViewHolder>() {
+class ChatAdapter(private val chatData: Array<String>) : RecyclerView.Adapter<ChatAdapter.MyViewHolder>() {
 
     class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MyViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    //Creates views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatAdapter.MyViewHolder {
+        val textView = LayoutInflater.from(parent.context).inflate(R.layout.my_text_view, parent, false) as TextView
+        return MyViewHolder(textView)
     }
 
-    override fun onBindViewHolder(p0: MyViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    //Replace contents of view
+    override fun onBindViewHolder(viewHolder: MyViewHolder, position: Int) {
+        viewHolder.textView.text = chatData[position]
     }
 
+    //Returns size of chatData
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return chatData.size
     }
 }
