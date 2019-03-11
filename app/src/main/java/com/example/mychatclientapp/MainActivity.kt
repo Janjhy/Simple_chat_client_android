fdf2587b3482f8fd3b642a90aad23e865930aa13
texacoson <jani.hyrkas@gmail.com>
2019-03-11 22:02:15 +0200

    Final editing

--- app/src/main/java/com/example/mychatclientapp/MainActivity.kt
+++ app/src/main/java/com/example/mychatclientapp/MainActivity.kt
@@ -2,16 +2,12 @@ package com.example.mychatclientapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import android.widget.EditText
-import java.io.BufferedWriter
-import java.io.OutputStreamWriter
 import java.io.PrintStream
-import java.io.PrintWriter
 import java.net.InetAddress
 import java.net.Socket
 import java.util.*
@@ -20,7 +16,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var out: PrintStream
     private lateinit var incoming: Scanner
-    //private lateinit var out1: PrintWriter
     private lateinit var socket: Socket
     private var chatData: ArrayList<String> = arrayListOf()
     private lateinit var myRecyclerView: RecyclerView
@@ -36,20 +31,11 @@ class MainActivity : AppCompatActivity() {
 
         Thread(ChatThread()).start()
 
-
         myRecyclerView = findViewById<RecyclerView>(R.id.myRecyclerView).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = theAdapter
         }
-
-        /*
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().run {
-                replace(R.id.chat_text_fragment, RecyclerViewFragment())
-                commit()
-            }
-        }*/
     }
 
     override fun onStart() {
@@ -61,31 +47,19 @@ class MainActivity : AppCompatActivity() {
     fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
-        //chatData.add(message)
 
-        /*var outThread = Thread(Runnable() {
-             fun run() {
-                try {
-                    Log.d("outThread", "run")
-                    out.println(message)
-                } catch (e: Exception) {
-                    println("Exception: ${e.message}")
-                }
-            }
-        })*/
         var outMessage = Thread(OutThread(message))
         outMessage.start()
-        //theAdapter.notifyDataSetChanged()
-        // RecyclerViewFragment().addMessage(message)
-
     }
 
+    //Needed to be able to update UI from a separated thread other than main thread.
     inner class AddMessage(): Runnable {
         override fun run() {
             theAdapter.notifyDataSetChanged()
         }
     }
 
+    //Used to run a separate thread to send message to the server.
     inner class OutThread(message: String): Runnable {
         private var message = message
 
@@ -99,12 +73,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Creates socket to the server and establishes a stream variable to send messages.
     inner class ChatThread() : Runnable {
         override fun run(){
             try {
                 var address: InetAddress = InetAddress.getByName("10.0.2.2")
                 socket = Socket(address, 30043)
-                //out1 = PrintWriter(BufferedWriter(OutputStreamWriter(socket.getOutputStream())), true)
                 out = PrintStream(socket.getOutputStream(), true)
             } catch (e: Exception) {
                 println("Exception: ${e.message} 2")
@@ -112,6 +86,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Initialises the variable to read from the server. Runs on a separate thread constantly reading from the server and updating UI.
     inner class UpdateChatThread(): Runnable {
         override fun run() {
             Log.d("update chat", "access")
@@ -121,24 +96,12 @@ class MainActivity : AppCompatActivity() {
                     chatData.add(incoming.nextLine())
                     runOnUiThread(AddMessage())
 
-
-                    //theAdapter.notifyDataSetChanged()
                     Log.d("update chat", "new incoming message")
 
                 } catch (e: Exception) {
                     println("Exception: ${e.message} 3")
                 }
             }
-
         }
     }
-
-    /*
-    private fun initChatData() {
-        //chatData = ArrayList(50) {i-> (i + 1).toString()}
-        chatData.add("First")
-        chatData.add("Second")
-        Log.d("initChat" , "chat initiated")
-    }
-    */
 }
